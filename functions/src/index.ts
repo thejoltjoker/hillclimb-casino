89cb83b2beb70ac4594b2df76a55fa9ea6c54341
Johannes Andersson <hello@thejoltjoker.com>
2023-12-19 16:27:27 +0000

    Add scores function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -22,6 +22,10 @@ import _ = require('lodash');
 //   response.send("Hello from Firebase!");
 // });
 
+const calculateScore = (bet: number, result: number, max = 100) => {
+	return max - Math.abs(bet - result);
+};
+
 admin.initializeApp();
 const db = admin.firestore();
 
@@ -40,5 +44,50 @@ exports.countResults = functions.firestore
 			});
 			result = _.mean(results);
 			roundRef.update({ result: result });
+		}
+	});
+
+exports.updateScores = functions.firestore
+	.document('rounds/{roundId}')
+	.onUpdate(async (change, context) => {
+		if (change.before.get('result') != change.after.get('result')) {
+			const batch = db.batch();
+			let scores = {};
+			const roundId = context.params.roundId;
+			logger.info('Updating leaderboard');
+
+			try {
+				const roundsSnapshot = await db.collection('rounds').get();
+
+				for (const round of roundsSnapshot.docs) {
+					const result = round.get('result');
+					if (round.id !== 'currentRound') {
+						const betsSnapshot = await round.ref.collection('bets').get();
+
+						betsSnapshot.forEach((bet) => {
+							const score = calculateScore(bet.data().value, result);
+
+							if (_.get(scores, bet.id)) {
+								scores = _.update({ ...scores }, bet.id, (value) => value + score);
+							} else {
+								_.set(scores, bet.id, score);
+							}
+						});
+					}
+				}
+
+				// Update scores collection
+				_.forIn(scores, (value, key) => {
+					logger.info('Score:', value, '@', key);
+					const docRef = db.doc(`scores/${key}`);
+					batch.set(docRef, { score: _.get(scores, key) });
+				});
+				// Commit scores
+				await batch.commit();
+
+				// Continue with batch commit or any other logic here
+			} catch (error) {
+				console.error('Error updating scores:', error);
+			}
 		}
 	});
